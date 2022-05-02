f92798c28497821bccf1ad6f990ac174d71c63aa
chenmoand <chenmoand@outlook.com>
2022-05-02 14:28:15 +0800

    add favicon

--- src/main/kotlin/runstatic/minecraft/listener/ServerMotdListener.kt
+++ src/main/kotlin/runstatic/minecraft/listener/ServerMotdListener.kt
@@ -4,9 +4,13 @@ import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.event.EventListener.Result
 import net.minestom.server.event.server.ServerListPingEvent
 import net.minestom.server.network.ConnectionManager
+import org.springframework.util.ResourceUtils
 import runstatic.minecraft.game.MinecraftEventListener
 import runstatic.minecraft.game.MinecraftListener
 import runstatic.minecraft.game.plus
+import runstatic.stools.logging.useSlf4jLogger
+import java.io.FileNotFoundException
+import java.util.*
 
 /**
  *
@@ -17,7 +21,22 @@ class ServerMotdListener(
     private val connectionManager: ConnectionManager
 ) : MinecraftEventListener<ServerListPingEvent>() {
 
-//    private val log = useSlf4jLogger()
+    private val log = useSlf4jLogger()
+
+    private val faviconBase64: String? by lazy {
+        try {
+            return@lazy "data:image/png;base64," + ResourceUtils.getFile("classpath:static/favicon.png").inputStream()
+                .use {
+                    Base64.getEncoder().encodeToString(it.readAllBytes())
+                }
+        } catch (e: FileNotFoundException) {
+            log.debug(e.message, e)
+            return@lazy null
+        } catch (e: Exception) {
+            log.error(e.message, e)
+            return@lazy null
+        }
+    }
 
     override fun run(event: ServerListPingEvent): Result {
         event.responseData.apply {
@@ -27,7 +46,7 @@ class ServerMotdListener(
                             (NamedTextColor.LIGHT_PURPLE + "wish you happy everyday!\n") +
                             (NamedTextColor.BLUE + "Server IP: mc.static.run")
                     )
-
+            favicon = faviconBase64
             addEntries(connectionManager.onlinePlayers)
         }
 
